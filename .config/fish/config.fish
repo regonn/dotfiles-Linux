b4a3f6b69b43465accf0f77a8386680c11e8032f
regonn <programmer@regonn.biz>
2017-07-13 13:50:49 +0900

    fcitx対応

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,13 @@ set -x LANG ja_JP.UTF-8
 set -x LC_ALL $LANG
 set -x PATH $PATH $HOME/.config/yarn/global/node_modules/.bin
 set -x PATH $PATH $HOME/.pyenv/bin
+set -x GTK_IM_MODULE fcitx
+set -x XMODIFIERS @im=fcitx
+set -x QT_IM_MODULE fcitx
 . (pyenv init - | psub)
 rbenv init - | source
 
+# Base16 Shell
+if status --is-interactive
+    eval sh $HOME/.config/base16-shell/scripts/base16-default-dark.sh
+end
