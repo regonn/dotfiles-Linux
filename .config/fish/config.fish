0fac2533c3d2a264fb5a3bacd4b562cf8c0cdb25
regonn <programmer@regonn.biz>
2017-08-03 12:40:39 +0900

    rubocopでruby version指定

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,12 @@
 set -x LANG ja_JP.UTF-8
 set -x LC_ALL $LANG
 set -x PATH $PATH $HOME/.config/yarn/global/node_modules/.bin
-set -x PATH $PATH $HOME/.pyenv/bin
 set -x GTK_IM_MODULE fcitx
 set -x XMODIFIERS @im=fcitx
 set -x QT_IM_MODULE fcitx
+set -gx PYENV_ROOT "$HOME/.pyenv"
+set -x PATH $PATH "$PYENV_ROOT/shims"
+status --is-interactive; and . (pyenv init - | psub)
 . (pyenv init - | psub)
 rbenv init - | source
 
