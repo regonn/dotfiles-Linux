a6be09b85ad4235d043f94be0573a0212e8a0c5b
regonn <programmer@regonn.biz>
2018-08-03 22:38:04 +0900

    rubocopはプロジェクト毎に設定するので削除

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,7 @@
 set -x LANG ja_JP.UTF-8
 set -x LC_ALL $LANG
 set -x PATH $PATH $HOME/.config/yarn/global/node_modules/.bin
-set -x PATH $PATH $HOME/.local/bin
+set -x PATH $PATH "/opt/flutter/bin"
 set -x GTK_IM_MODULE fcitx
 set -x XMODIFIERS @im=fcitx
 set -x DOTNET_SKIP_FIRST_TIME_EXPERIENCE true
@@ -9,11 +9,11 @@ set -x QT_IM_MODULE fcitx
 set -gx PYENV_ROOT "$HOME/.pyenv"
 set -x PATH $PATH "$PYENV_ROOT/shims"
 set -x PATH $PATH "$HOME/.pub-cache/bin"
-set -x PATH $PATH "$HOME/flutter/flutter/bin"
 set -x XGBOOST_BUILD_VERSION master
 set -x JULIA_HOME "/usr/bin"
 set -x LIBJULIA "/usr/lib/libjulia.so"
-set -x ANDROID_HOME "/opt/android-sdk"
+set -x ANDROID_SDK_ROOT "$HOME/Android/sdk"
+set -x ANDROID_HOME "$ANDROID_SDK_ROOT"
 status --is-interactive; and . (pyenv init - | psub)
 rbenv init - | source
 
--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -2,6 +2,6 @@
 ln -s ~/dotfiles-linux/.zshrc ~/.zshrc
 ln -s ~/dotfiles-linux/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles-linux/.xprofile ~/.xprofile
-ln -s ~/dotfiles-linux/.rubocop.yml ~/.rubocop.yml
 ln -s ~/dotfiles-linux/.config/fish/config.fish ~/.config/fish/config.fish
 ln -s ~/dotfiles-linux/.eslintrc.yml ~/.eslintrc.yml
+ln -s ~/dotfiles-linux/.config/rubymine.dic ~/.config/rubymine.dic
